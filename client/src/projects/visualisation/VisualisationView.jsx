24423274b782f1a7f60f0f4da216a319e4f5b15e
Riordan Dervin Alfredo <riordan.alfredo@gmail.com>
2022-08-22 17:39:26 +1000

    add "missing visualisation" message if there's missing files or visualisations are incorrect

--- client/src/projects/visualisation/VisualisationView.jsx
+++ client/src/projects/visualisation/VisualisationView.jsx
@@ -8,6 +8,7 @@ import { Button, ButtonGroup, Carousel, Image } from "react-bootstrap";
 const VisualisationView = () => {
   const { simulationId } = useParams();
   const [index, setIndex] = useState(0);
+  const [hasVisualisation, setHasVisualisation] = useState(true);
 
   const styles = {
     outer: {
@@ -16,13 +17,13 @@ const VisualisationView = () => {
       height: "100%",
       colour: "white",
     },
-    carousel: { height: "90vh", width:"85vw"},
+    carousel: { height: "90vh", width: "85vw" },
     middle: {
       display: "flex",
       justifyContent: "center",
       alignItems: "center",
       height: "90vh",
-    }
+    },
   };
   const csvUrl = process.env.PUBLIC_URL + "/api/visualisations/" + simulationId;
 
@@ -30,9 +31,15 @@ const VisualisationView = () => {
     {
       id: "teamwork-barchart",
       label: "Teamwork",
-      view: <div style={styles.middle}>
-        <Image width={"850px"} src={`${csvUrl}/teamwork-barchart`} /> 
-        </div>,
+      view: (
+        <div style={styles.middle}>
+          <Image
+            width={"850px"}
+            src={`${csvUrl}/teamwork-barchart`}
+            onError={() => setHasVisualisation(false)}
+          />
+        </div>
+      ),
     },
     {
       id: "hive-button",
@@ -42,7 +49,15 @@ const VisualisationView = () => {
     {
       id: "audio-socnet",
       label: "Speaking Interaction",
-      view: <div style={styles.middle}><Image width={"700px"}  src={`${csvUrl}/audio-socnet`} /> </div>,
+      view: (
+        <div style={styles.middle}>
+          <Image
+            width={"700px"}
+            src={`${csvUrl}/audio-socnet`}
+            onError={() => setHasVisualisation(false)}
+          />{" "}
+        </div>
+      ),
     },
   ];
 
@@ -53,25 +68,45 @@ const VisualisationView = () => {
   return (
     <div style={styles.outer}>
       {/* <h3>Session {simulationId}</h3> */}
-      <ButtonGroup>
-        {visualisations.map((d, index) => {
-          return (
-            <Button id={d.id} onClick={() => handleSelect(index)}>
-              {d.label}
-            </Button>
-          );
-        })}
-      </ButtonGroup>
-      <Carousel
-        activeIndex={index}
-        onSelect={handleSelect}
-        style={styles.carousel}
-        interval={null}
-      >
-        {visualisations.map((d) => (
-          <Carousel.Item> {d.view}</Carousel.Item>
-        ))}
-      </Carousel>
+      {!hasVisualisation ? (
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            height: "100%",
+          }}
+        >
+          <h1>No Visualisation 🚩</h1>
+          <p>
+            Visualisations are not available due to incorrect data or missing
+            files.
+          </p>
+        </div>
+      ) : (
+        <div>
+          <ButtonGroup>
+            {visualisations.map((d, index) => {
+              return (
+                <Button id={d.id} onClick={() => handleSelect(index)}>
+                  {d.label}
+                </Button>
+              );
+            })}
+          </ButtonGroup>
+          <Carousel
+            activeIndex={index}
+            onSelect={handleSelect}
+            style={styles.carousel}
+            interval={null}
+          >
+            {visualisations.map((d) => (
+              <Carousel.Item> {d.view}</Carousel.Item>
+            ))}
+          </Carousel>
+        </div>
+      )}
+
       <ReactTooltip />
     </div>
   );
