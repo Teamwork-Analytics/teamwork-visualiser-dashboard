ce7226a8a3edfb339cb46d1eaaa8efcd86842835
Jie Fan <jxiangfan@gmail.com>
2023-08-31 08:39:45 +1000

    removed dead files

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import SimulationPage from "../pages/simulation/SimulationPage";
