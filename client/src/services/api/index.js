968e201ebc4b30a91b974fb48f62b4a6ddbd82a4
Jie Fan <jxiangfan@gmail.com>
2023-08-31 11:01:19 +1000

    trying to access private ip

--- client/src/services/api/index.js
+++ client/src/services/api/index.js
@@ -9,7 +9,8 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 const usingRp = true;
-const localServerUrl = "http://13.236.94.219:5002/api";
+const localServerUrl = `${process.env.REACT_APP_EXPRESS_IP}:${process.env.REACT_APP_EXPRESS_PORT}/api`;
+console.log("localServerUrl", localServerUrl);
 const api = axios.create({
   baseURL: localServerUrl,
   withCredentials: false,
