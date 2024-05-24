562557151ab9a72e338cff75d04f7d0218db270d
paola-md <paola.mejia@epfl.ch>
2024-05-24 16:32:13 +1000

    adapts the text based on the x-axis

--- py-server/coteaching/textDescriptions.py
+++ py-server/coteaching/textDescriptions.py
@@ -25,7 +25,7 @@ def get_ta_pairs(current_TA, ta_list = ['B', 'G', 'R']):
     return partner1, partner2, pair1, pair2
 
 
-def get_max_coteaching(pair1, partner1, df_time):
+def get_max_coteaching(pair1, partner1, df_time, spatial_type = 'None'):
     one_pair = df_time[f'{pair1}_coteaching'].value_counts()/len(df_time)
     one_pair = pd.DataFrame(one_pair).reset_index()
     one_pair.columns = ['coteaching', 'percentage']
@@ -36,6 +36,16 @@ def get_max_coteaching(pair1, partner1, df_time):
 
     if one_pair.shape[0] == 0:
         return 0, 'None'
+    
+    # All of them have lecturing. So if it is lecturing or None, nothing changes. 
+    if spatial_type == 'supervisory':
+        one_pair = one_pair.query('coteaching == "One Teacher, One Assistant"' ).copy() 
+    elif spatial_type == 'interaction':
+        one_pair = one_pair.query('coteaching ==  "One Teacher, One Assistant" or coteaching == "Alternative Teaching"').copy() 
+    elif spatial_type == 'personal':
+        one_pair = one_pair.query('coteaching ==  "One Teacher, One Observer"').copy() 
+
+    one_pair.sort_values('percentage', ascending=False, inplace=True)
     one_pair_max = one_pair.iloc[0]
     one_pair_max_perc = one_pair_max['percentage']
     one_pair_max_coteaching = one_pair_max['coteaching']
@@ -80,35 +90,44 @@ def get_text_description_coteaching_strategy(one_pair_max_coteaching, one_pair_m
     return text
 
 
-def get_text_description_all(df_time):
-    bg_perc, bg_coteaching = get_max_coteaching('BG', '', df_time)
-    br_perc, br_coteaching = get_max_coteaching('BR', '', df_time)
-    gr_perc, gr_coteaching = get_max_coteaching('GR', '', df_time)
+def get_text_description_all(df_time, spatial_type ):
+    bg_perc, bg_coteaching = get_max_coteaching('BG', '', df_time,spatial_type)
+    br_perc, br_coteaching = get_max_coteaching('BR', '', df_time,spatial_type)
+    gr_perc, gr_coteaching = get_max_coteaching('GR', '', df_time,spatial_type)
 
     bgr_coteaching = pd.DataFrame({'pair': ['BG', 'BR', 'GR'], 
                 'percentage': [bg_perc, br_perc, gr_perc], 
                 'coteaching': [bg_coteaching, br_coteaching, gr_coteaching],
-                'names': ["Blue and Green TA", "Blue and Red TA", "Green and Red TA"]})
+                'names': ["<b>Blue</b> and <b>Green</b> TA", 
+                          "<b>Blue</b> and <b>Red</b> TA",
+                          "<b>Green</b> and <b>Red</b> TA"]})
 
-    bgr_coteaching.sort_values('percentage', ascending=False, inplace=True)
     bgr_coteaching['percentage'] = round(bgr_coteaching['percentage']*100).astype(int)
     bgr_coteaching = bgr_coteaching.query('coteaching != "None"').query('percentage > 0').copy()
 
+    if type == 'None': #Give most common strategy
+        bgr_coteaching.sort_values('percentage', ascending=False, inplace=True)
+    
+
     text = "Error in text description all"
     ### Case 1: They are all missing
+    extra_text = ''
+    if spatial_type != 'None':
+        extra_text= ' and category'
+
     if (bg_coteaching == 'None') & (br_coteaching == 'None') & (gr_coteaching == 'None'):
-        text = "During this period, we did not identify any co-teaching strategies with the other TAs."
+        text = f"During this period{extra_text}, we did not identify any co-teaching strategies with the other TAs."
 
     ### Case 2: There's only one present
     elif len(bgr_coteaching) > 1: 
         strategy = bgr_coteaching.iloc[0]['coteaching']
-        text = f"""The most common co-teaching strategy was 
+        text = f"""During this period{extra_text}, the most common co-teaching strategy was 
         <a
         data-tip data-for='{strategy.lower().replace(',','').replace(' ', '-')}'>
         <i><b>{strategy}</b></i>
         </a>, 
         which was used {bgr_coteaching.iloc[0]['percentage']}% of the time 
-        by the <b>{bgr_coteaching.iloc[0]['names']}</b> TA. """ 
+        by the {bgr_coteaching.iloc[0]['names']} TA. """ 
 
 
     text.replace('\n', '')
@@ -116,12 +135,12 @@ def get_text_description_all(df_time):
 
     return text 
 
-def get_text(df_time, current_TA='ALL', type = 'ALL'):
+def get_text(df_time, current_TA='ALL', spatial_type = 'None'):
     # Type: authoritative, supervisory, interactional, personal
     text = "Error"
 
     if current_TA == 'ALL':
-        text = get_text_description_all(df_time)
+        text = get_text_description_all(df_time, spatial_type)
     else:
         partner1, partner2, pair1, pair2 = get_ta_pairs(COLOUR_MAPPING[current_TA])
         one_pair_max_perc, one_pair_max_coteaching = get_max_coteaching(pair1, partner1, df_time)
