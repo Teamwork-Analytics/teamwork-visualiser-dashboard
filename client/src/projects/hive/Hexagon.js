ae1e82d49a18dc8ddb0dd37540d66f7ac0a28e1d
Riordan Alfredo <riordan.alfredo@gmail.com>
2024-06-11 15:14:12 +1000

    Hive with heart-rate

--- client/src/projects/hive/Hexagon.js
+++ client/src/projects/hive/Hexagon.js
@@ -63,11 +63,33 @@ class HexagonComponent {
               );
             } else if (record.audio === "0") {
               this.render([posX, posY], "missed", record.tagId);
-            } else if (record.audio === "2") {
-              this.render([posX, posY], "fixed", record.tagId, record.tracker);
             }
           }
         });
+
+        let hasHeartRateKey = clean.some((obj) =>
+          obj.hasOwnProperty("heartRate")
+        );
+
+        if (hasHeartRateKey) {
+          let hrBaselines = this.getBaselineHeartRate(d); // always get from first data.
+          const hrData = this.constructHeartRateData(clean, hrBaselines); // only 4 data
+          console.log(hrData);
+
+          hrData.forEach((record) => {
+            const posX =
+              (record.x * CONSTANTS.IMG_WIDTH) / CLASSROOM_SIZE.WIDTH;
+            const posY =
+              (record.y * CONSTANTS.IMG_HEIGHT) / CLASSROOM_SIZE.HEIGHT;
+
+            this.render(
+              [posX, posY],
+              "fixed",
+              record.tagId, // Colour
+              record.heartRate // value: maximuma heart rate
+            );
+          });
+        }
       }.bind(this)
     );
 
@@ -78,12 +100,52 @@ class HexagonComponent {
     //     .attr("fill", "white");
   }
 
-  render(subjectPos, shotFlag, colour, number) {
+  getBaselineHeartRate(data) {
+    let tagIds = ["RED", "GREEN", "BLUE", "YELLOW"];
+    let result = {};
+
+    for (let tagId of tagIds) {
+      let firstItem = data.find((item) => item.tagId === tagId);
+      if (firstItem) {
+        result[tagId] = Number(firstItem.heartRate);
+      }
+    }
+    return result;
+  }
+
+  constructHeartRateData(data, baselines) {
+    let maxHeartRates = data.reduce((acc, obj) => {
+      const hr = Number(obj.heartRate);
+      if (!acc[obj.tagId] || hr > Number(acc[obj.tagId].heartRate)) {
+        const baseline = baselines[obj.tagId];
+        // NOTE: Changes from baseline
+        // acc[obj.tagId] = {
+        //   ...obj,
+        //   heartRate: obj.heartRate - baseline,
+        //   baselineHr: baseline,
+        // };
+
+        // just Maximum
+        acc[obj.tagId] = {
+          ...obj,
+          baselineHr: baseline,
+        };
+      }
+      return acc;
+    }, {});
+    let res = Object.entries(maxHeartRates).map(([tagId, value]) => ({
+      tagId,
+      ...value,
+    }));
+    return res;
+  }
+
+  render(subjectPos, shotFlag, colour, value) {
     const hexbin = d3hex.hexbin().radius(CONSTANTS.HEX_RADIUS);
-    const h2 = d3hex.hexbin().radius(30);
+    const h2 = d3hex.hexbin().radius(100);
     const strokeWidth = "0.15em";
     const strokeColour = shotFlag === "made" ? null : cssColourMatcher[colour];
-    if (!!shotFlag) {
+    if (shotFlag === "made" || shotFlag === "missed") {
       this.svg
         .append("g")
         .attr("transform", `translate(0, ${CONSTANTS.IMG_HEIGHT}) scale(1,-1)`)
@@ -110,28 +172,53 @@ class HexagonComponent {
       // })
       // .style("opacity", 1);
     } else if (shotFlag === "fixed") {
+      // this.svg
+      //   .append("g")
+      //   .attr("transform", `translate(0, ${CONSTANTS.IMG_HEIGHT}) scale(1,-1)`)
+      //   .selectAll(".hexagon")
+      //   .data(hexbin([subjectPos]))
+      //   .enter()
+      //   .append("path")
+      //   .attr("d", function (d) {
+      //     const x = -d.y + CONSTANTS.IMG_WIDTH;
+      //     const y = d.x;
+      //     // const x = d.x;
+      //     // const y = d.y;
+      //     return "M" + x + "," + y + h2.hexagon();
+      //   })
+      //   .attr("stroke", "black")
+      //   .attr("fill", cssColourMatcher[colour])
+      //   .attr("fill-opacity", "0.7")
+      //   .attr("stroke-width", "1em");
+      const posX = CONSTANTS.IMG_WIDTH - subjectPos[1];
+      const posY = CONSTANTS.IMG_HEIGHT - subjectPos[0];
+      let heartPath =
+        "M12 4.248c-3.148-5.402-12-3.825-12 2.944 0 4.661 5.571 9.427 12 15.808 6.43-6.381 12-11.147 12-15.808 0-6.792-8.875-8.306-12-2.944z";
+
+      const heartPosX = posX - 110;
+      const heartPosY = posY - 120;
+
       this.svg
         .append("g")
-        .selectAll(".hexagon")
-        .data(hexbin([subjectPos]))
-        .enter()
         .append("path")
-        .attr("d", function (d) {
-          return "M" + d.x + "," + d.y + h2.hexagon();
-        })
-        .attr("stroke", "white")
-        .attr("fill", "yellow")
-        .attr("fill-opacity", "0.7")
-        .attr("stroke-width", strokeWidth);
+        .attr("d", heartPath)
+        .attr("fill", cssColourMatcher[colour])
+        .attr("fill-opacity", "0.8")
+        .attr("stroke", "black")
+        .attr("stroke-width", "0.1em")
+        .style(
+          "transform",
+          `translate(${heartPosX}px, ${heartPosY}px) scale(10)`
+        );
 
       // FOR TEXT
       this.svg
         .append("text")
-        .text(number)
-        .attr("x", subjectPos[0] + 10)
-        .attr("y", subjectPos[1] + 5)
-        .attr("fill", "white")
-        .style("font-size", "20px");
+        .text(value)
+        .attr("x", posX - 2 * (posX / 100))
+        .attr("y", posY + 2 * (posY / 100))
+        .attr("fill", "black")
+        .style("font-size", "5em");
     }
   }
 }
