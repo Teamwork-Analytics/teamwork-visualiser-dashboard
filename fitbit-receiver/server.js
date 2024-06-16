08047bb5c1afec66d9c3064fc4a3dd244511128f
Jie <jxiangfan@gmail.com>
2024-06-16 23:09:37 +1000

    fitbit server to recognise session

--- fitbit-receiver/server.js
+++ fitbit-receiver/server.js
@@ -5,9 +5,35 @@ const dataHandler = require("./dataHandler");
 
 app.use(express.json()); // for parsing application/json
 
+let simulationId = null;
+
+// Endpoint to receive the simulationId
+app.post("/start-simulation", (req, res) => {
+  simulationId = req.body.simulationId;
+  console.log("Received simulationId:", simulationId);
+  res.status(200).send("Simulation ID received");
+});
+
+// Endpoint to stop the simulation and clear the simulationId
+app.post("/stop-simulation", (req, res) => {
+  if (!simulationId) {
+    res.status(400).send("No active simulation to stop");
+    return;
+  }
+  console.log("Stopping simulation with ID:", simulationId);
+  simulationId = null;
+  res.status(200).send("Simulation stopped and ID cleared");
+});
+
 app.post("/data", (req, res) => {
+  if (!simulationId) {
+    res
+      .status(400)
+      .send("Simulation ID not received OR simulation not started yet");
+    return;
+  }
   console.log("Received data:", req.body);
-  dataHandler.handleReceivedData(req.body);
+  dataHandler.handleReceivedData(req.body, simulationId);
   res.status(200).send("Data received");
 });
 
