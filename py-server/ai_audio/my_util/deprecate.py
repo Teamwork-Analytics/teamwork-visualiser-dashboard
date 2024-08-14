de46dc04a822759d15752b1b8c300f4dfbd9b244
Riordan Alfredo <riordan.alfredo@gmail.com>
2024-08-14 12:21:00 +1000

    untrack unused python system
    the files might be too big too!

--- py-server/ai_audio/my_util/deprecate.py
+++
@@ -1,8 +0,0 @@
-
-
-def deprecated(func):
-    def wrap_func(*args, **kwargs):
-        print(f'===== Function {func.__name__!r} is deprecated')
-        return func(*args, **kwargs)
-
-    return wrap_func
--- py-server/ai_audio/my_util/dummy.bat
+++
@@ -1,2 +0,0 @@
-ECHO HI
-ECHO BYE
