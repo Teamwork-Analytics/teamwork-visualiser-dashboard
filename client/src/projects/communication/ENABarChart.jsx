316ed59bc72d40d91eb36c185e850657c4bb8509
Linx.Zhao <zhaolinx123@163.com>
2025-10-02 18:32:44 +1000

    Initial commit for Barchart for ENA

---
+++ client/src/projects/communication/ENABarChart.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getTeamworkBarchart } from "../../services/py-server/indexVisualiser";
+import Barchart from "../teamwork-prio/Barchart";
+import SimpleErrorText from "../../components/errors/ErrorMessage";
+import { Chart as ChartJS, registerables } from "chart.js";
+ChartJS.register(...registerables);
+
+const SNABarChart = ({
+  height,
+  width,
+  timeRange,
+  yLabelsFontSize,
+  customAspectRatio,
+}) => {
+  const { simulationId } = useParams();
+  const [snaCountData, setSnaCountData] = useState([]);
+  const [isError, setIsError] = useState(snaCountData.length === 0);
+
+  const startTime = timeRange[0];
+  const endTime = timeRange[1];
+
+  useEffect(() => {
+    getTeamworkBarchart({
+      simulationId: simulationId,
+      startTime: startTime,
+      endTime: endTime,
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          const filteredData = res.data;
+          const finalData = filteredData.filter(
+            (d) => d["label"][0] !== "Moving around"
+          );
+          setSnaCountData(finalData);
+          setIsError(false);
+        }
+      })
+      .catch((e) => {
+        setIsError(true);
+        console.error(e);
+        // toast.error("Teamwork Barchart error");
+      });
+  }, [simulationId, startTime, endTime]);
+
+  useEffect(() => {
+    if (snaCountData.length === 0) {
+      // Fetch data immediately when component mounts
+      function fetchData() {
+        getTeamworkBarchart({
+          simulationId: simulationId,
+          startTime: startTime,
+          endTime: endTime,
+        })
+          .then((res) => {
+            if (res.status === 200) {
+              setSnaCountData(res.data);
+              setIsError(false);
+            }
+          })
+          .catch((e) => {
+            setIsError(true);
+            console.error(e);
+            // toast.error("Teamwork Barchart error");
+          });
+      }
+
+      // Set up interval to fetch data every X milliseconds. Here, we use 5000ms (5 seconds) as an example.
+      const intervalId = setInterval(fetchData, 10000);
+
+      // Clean up the interval when the component is unmounted or when data is fetched
+      return () => clearInterval(intervalId);
+    }
+  }, [endTime, simulationId, startTime, snaCountData]);
+
+  return (
+    <SimpleErrorText isError={isError} message={"Tool in preparation."}>
+      <Barchart
+        data={snaCountData}
+        height={height}
+        width={width}
+        yLabelsFontSize={yLabelsFontSize}
+        customAspectRatio={customAspectRatio}
+      />
+    </SimpleErrorText>
+  );
+};
+
+export default SNABarChart;
